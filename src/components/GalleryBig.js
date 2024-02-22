950c4f31c8ce6b9b23d0923ca26aaeb289a5896d
Ekaterina Grishina <grischina.kotya17@gmail.com>
2024-02-22 12:35:41 +0100

    Add css-animation in component GalleryBig

--- src/components/GalleryBig.js
+++ src/components/GalleryBig.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { Link } from "react-router-dom";
 import { useState } from 'react';
 
 export default function GallerryBig() {
-    // const styleGallery = {
-    //     transform: 'translateX(0);'
-    // }
-    // const [position, setPosition] = useState(styleGallery);
-
-    // function handleClick() {
-    //     setPosition({
-    //         transform: 'translateX(-300px)'
-    //     })
-    // }
-
-    let countLeft = 0;
+    const [count, setCount] = useState(0);
     const [position, setPosition] = useState(0);
 
-    function handleClick() {
-        ++countLeft;
-        setPosition(countLeft * -300);
-        console.log(countLeft, position);
+    function handleClickLeft() {
+        setCount(count + 1);
+        setPosition(position - 300);
+    }
+
+    function handleClickRight() {
+        setCount(count - 1);
+        setPosition(position + 300);
     }
 
     return (
@@ -28,23 +21,28 @@ export default function GallerryBig() {
             <div className='gallery-big-title'>
                 <h3>подарочные <span className='blue'>сертификаты</span></h3>
                 <div className='gallery-btn'>
-                    <button className='circle-btn' id='left' disabled>
+                    <button className='circle-btn' id='left'
+                        disabled={count < 1}
+                        onClick={handleClickRight}>
                         <i className='fas fa-arrow-right'></i>
                     </button>
-                    <button className='circle-btn' id='right' 
-                        onClick={handleClick}>
+                    <button className='circle-btn' id='right'
+                        disabled={count >= 3}
+                        onClick={handleClickLeft}>
                         <i className='fas fa-arrow-right'></i>
                     </button>
                 </div>
             </div>
-            <div className='gallery-big-photos' style={{ transform: `translateX(${position}px)` }}>
+
+            <div className='gallery-big-photos'
+                style={{ transform: `translateX(${position}px)` }}>
                 <div>
                     <img src='./images/gallery-big1.png' alt=''></img>
                     <img src='./images/gallery-big2.png' alt=''></img>
                     <div className='gallery-photo-textblock'>
                         <h4>Внесите краски в свою жизнь и в жизнь своих близких</h4>
                         <p>У нас вы можете преобрести подарочные сертификаты на любую из предоставляемых услуг. Подарите своим близким незабываемые морские впечатления!</p>
-                        <button className='main-btn'>Оформить сертификат</button>
+                        <Link to='/certificates'><button className='main-btn'>Оформить сертификат</button></Link>
                     </div>
                 </div>
                 <img src='./images/gallery-big3.png' alt='' className='tall'></img>

c80ae0d073dbd798cf4ba46386b0ff8764d73e2a
Ekaterina Grishina <grischina.kotya17@gmail.com>
2024-02-22 21:11:46 +0100

    Create new component Form and add image

---
+++ src/components/Form.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import Input from 'react-phone-number-input/input'
+
+export default function Form() {
+    const [value, setValue] = useState();
+    const [errorInput, setErrorInput] = useState({ border: "none" });
+
+    // function handleBlur(e) {
+    //     let regexp = /./;
+    //     if (!e.value.includes(regexp)) {
+    //         setErrorInput({ border: "1px solif #D00000" });
+    //     } else {
+    //         setErrorInput({ border: "none" });
+    //     }
+    // }
+
+    return (
+        <div className="form-container">
+            <div className="form-right">
+                <h3>Заполните форму и получите <br></br><span className="blue">скидку на первое посещение</span></h3>
+                <img src="./images/img-form.png" alt=""></img>
+            </div>
+
+            <form name="form" action="" method="get" className="form-left">
+                <div>
+                    <label for="name">Ваше имя*</label>
+                    <input type="text" name="name" id="name" placeholder="Имя" required style={errorInput}></input>
+
+                    <label for="lastname">Ваша фамилия*</label>
+                    <input type="text" name="lastname" id="lastname" placeholder="Фамилия" required></input>
+
+                    <div className="form-row">
+                        <div className="form-row-item">
+                            <label for="phone">Номер телефона*</label>
+                            <Input
+                                placeholder="+7(     )     -    - "
+                                // country="RU"
+                                // international
+                                // withCountryCallingCode
+                                required
+                                maxlength="16"
+                                value={value}
+                                onChange={setValue} />
+                        </div>
+                        <div className="form-row-item">
+                            <label for="email">Электронная почта*</label>
+                            <input type="email" name="email" id="email" placeholder="Адрес эл. почты" required></input>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="form-row">
+                    <p>Я принимаю <Link to="/privacy"><span>политику обработки персональных данных</span></Link></p>
+                    <button className="main-btn" id="submit">Отправить форму</button>
+                    {/* добавить функционал по сохранению формы в локале */}
+                </div>
+            </form>
+        </div>
+    )
+}

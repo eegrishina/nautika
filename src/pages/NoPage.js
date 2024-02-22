b1497069e63efad50e47f380f42e2f48c658d643
Ekaterina Grishina <grischina.kotya17@gmail.com>
2024-02-22 11:42:01 +0100

    Delete component Page404

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import Page404 from '../components/Page404';
+import { Link } from "react-router-dom";
 
 export default function NoPage() {
     return (
-        <Page404 />
+        <div className='container'>
+            <div className='main-info'>
+                <div className='main-column'>
+                    <small>Кажется, вы сбились с курса. Но не переживайте, <br></br>мы готовы помочь вам!</small>
+                    <img className='main-img' src='./images/404upd.png' alt=''></img>
+                    <button className='line-btn'><Link to="/">Вернуться на главную</Link></button>
+                </div>
+            </div>
+        </div>
     )
 }

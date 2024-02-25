1396a8b992c66d0b316e9b7040f0d5f003889a59
Ekaterina Grishina <grischina.kotya17@gmail.com>
2024-02-25 17:37:00 +0100

    Edit style for PriceOneCard

--- src/components/PriceOneCard.js
+++ src/components/PriceOneCard.js
@@ -5,16 +5,19 @@ import { Link } from "react-router-dom";
 export default function PriceOneCard({ cardTitle, cardText, cardPrice, cardTime, cardBtn }) {
     const [hoverClass, setHoverClass] = useState(false);
     const [size, setSize] = useState("30px");
+    const [opacity, setOpacity] = useState(0);
 
     return (
         <div className="card-container"
             onMouseOver={() => {
                 setHoverClass(true)
                 setSize("40px")
+                setOpacity(1)
             }}
             onMouseOut={() => {
                 setHoverClass(false)
                 setSize("30px")
+                setOpacity(0)
             }}
         >
             <h5 style={{ fontSize: size }}>{cardTitle}</h5>
@@ -24,7 +27,7 @@ export default function PriceOneCard({ cardTitle, cardText, cardPrice, cardTime,
                     <div className="pill pill-price">{cardPrice}</div>
                     <div className="pill pill-time">{cardTime}</div>
                 </div>
-                {hoverClass && <button className="btn-red-arrow"><Link to='/training'>{cardBtn} <i className="fas fa-arrow-right"></i></Link></button>}
+                {hoverClass && <button className="btn-red-arrow" style={{ opacity: opacity }}><Link to='/training'>{cardBtn} <i className="fas fa-arrow-right"></i></Link></button>}
             </div>
         </div>
     )

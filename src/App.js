354bd9fe6945a68ce8ffec44617e941ad6fd8230
Ekaterina Grishina <grischina.kotya17@gmail.com>
2024-03-05 18:56:23 +0700

    Edit main component App (delete image paths)

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ import About from './pages/About';
 import Thanks from './pages/Thanks';
 import NoPage from './pages/NoPage';
 import Contacts from './pages/Contacts';
-import {api} from './api';
 
 export default function App({ path }) {
   document.addEventListener('click', (e) => {
@@ -20,32 +19,21 @@ export default function App({ path }) {
     if (!link) return;
     window.scrollTo(0, 0);
   })
-  
-  function handleImage() {
-    api.map(item => {
-      if (item.id === path) {
-        return item.imgTitle;
-      }
-      if (path === 'home') {
-        return '/public/images/main-ship.png';
-      }
-    })
-  }
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home imgPath={handleImage} />} />
-          <Route path="services" element={<Services imgPath={handleImage} />} />
-          <Route path="education" element={<Education imgPath={handleImage} />} />
-          <Route path="rent" element={<Rent imgPath={handleImage} />} />
-          <Route path="walks" element={<Walks imgPath={handleImage} />} />
-          <Route path="certificates" element={<Certificates imgPath={handleImage} />} />
-          <Route path="about" element={<About imgPath={handleImage} />} />
-          <Route path="thanks" element={<Thanks imgPath={handleImage} />} />
-          <Route path="contacts" element={<Contacts imgPath={handleImage} />} />
-          <Route path="*" element={<NoPage imgPath={handleImage} />} />
+          <Route index element={<Home />} />
+          <Route path="services" element={<Services />} />
+          <Route path="education" element={<Education />} />
+          <Route path="rent" element={<Rent />} />
+          <Route path="walks" element={<Walks />} />
+          <Route path="certificates" element={<Certificates />} />
+          <Route path="about" element={<About />} />
+          <Route path="thanks" element={<Thanks />} />
+          <Route path="contacts" element={<Contacts />} />
+          <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
     </BrowserRouter>

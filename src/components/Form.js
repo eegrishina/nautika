e8ebe1cc033c70019379d8d90e545695438a4c51
Ekaterina Grishina <grischina.kotya17@gmail.com>
2024-03-05 14:49:11 +0700

    Change component Form specially for the form (without interactive)

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,8 @@ import { Link } from "react-router-dom";
 import Input from 'react-phone-number-input/input'
 
 export default function Form() {
-    const [value, setValue] = useState();
+    const [phone, setPhone] = useState();
+    const [email, setEmail] = useState("");
     const [errorInput, setErrorInput] = useState({ border: "none" });
 
     // function handleBlur(e) {
@@ -17,46 +18,40 @@ export default function Form() {
     // }
 
     return (
-        <div className="form-container">
-            <div className="form-right">
-                <h3>Заполните форму и получите <br></br><span className="blue">скидку на первое посещение</span></h3>
-                <img src="./images/img-form.png" alt=""></img>
-            </div>
+        <form name="form" action="" method="get">
+            <div>
+                <label htmlFor="name">Ваше имя*</label>
+                <input type="text" name="name" id="name" placeholder="Имя" required style={errorInput}></input>
 
-            <form name="form" action="" method="get" className="form-left">
-                <div>
-                    <label htmlFor="name">Ваше имя*</label>
-                    <input type="text" name="name" id="name" placeholder="Имя" required style={errorInput}></input>
-
-                    <label htmlFor="lastname">Ваша фамилия*</label>
-                    <input type="text" name="lastname" id="lastname" placeholder="Фамилия" required></input>
-
-                    <div className="form-row">
-                        <div className="form-row-item">
-                            <label htmlFor="phone">Номер телефона*</label>
-                            <Input
-                                placeholder="+7(     )     -    - "
-                                // country="RU"
-                                // international
-                                // withCountryCallingCode
-                                required
-                                maxLength="16"
-                                value={value}
-                                onChange={setValue} />
-                        </div>
-                        <div className="form-row-item">
-                            <label htmlFor="email">Электронная почта*</label>
-                            <input type="email" name="email" id="email" placeholder="Адрес эл. почты" required></input>
-                        </div>
-                    </div>
-                </div>
+                <label htmlFor="lastname">Ваша фамилия*</label>
+                <input type="text" name="lastname" id="lastname" placeholder="Фамилия" required></input>
 
                 <div className="form-row">
-                    <p>Я принимаю <Link to="/privacy"><span>политику обработки персональных данных</span></Link></p>
-                    <button className="main-btn" id="submit">Отправить форму</button>
-                    {/* добавить функционал по сохранению формы в локале */}
+                    <div className="form-row-item">
+                        <label htmlFor="phone">Номер телефона*</label>
+                        <Input
+                            placeholder="+7(     )     -    - "
+                            // country="RU"
+                            // international
+                            // withCountryCallingCode
+                            required
+                            maxLength="16"
+                            value={phone}
+                            onChange={setPhone} />
+                    </div>
+                    <div className="form-row-item">
+                        <label htmlFor="email">Электронная почта*</label>
+                        <input type="email" name="email" id="email" placeholder="Адрес эл. почты" required
+                            value={email} onChange={setEmail}></input>
+                    </div>
                 </div>
-            </form>
-        </div>
+            </div>
+
+            <div className="form-row form-row-btns">
+                <p>Я принимаю <Link to="/privacy"><span>политику обработки персональных данных</span></Link></p>
+                <button className="main-btn" id="submit">Отправить форму</button>
+                {/* добавить функционал по сохранению формы в локале */}
+            </div>
+        </form>
     )
 }

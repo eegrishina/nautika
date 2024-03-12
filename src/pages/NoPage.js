04780c598613819788a5e71d157e0c6a5b8d7733
Ekaterina Grishina <grischina.kotya17@gmail.com>
2024-03-12 17:39:33 +0700

    Change page NoPage with component FirstScreen

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import FirstScreen from '../components/FirstScreen';
 
 export default function NoPage() {
     return (
         <div className='container'>
-            <div className='main-info'>
-                <div className='main-column'>
-                    <small>Кажется, вы сбились с курса. Но не переживайте, мы готовы помочь вам!</small>
-                    <img className='main-img' src='./images/nopage.png' alt=''></img>
-                    <button className='line-btn'><Link to="/">Вернуться на главную</Link></button>
-                </div>
-            </div>
+            <FirstScreen imgPath='./images/nopage.png'
+                small="Кажется, вы сбились с курса. Но не переживайте, мы готовы помочь вам!"
+                btn="Вернуться на главную"
+                btnStyle="btn-red-arrow" />
         </div>
     )
 }

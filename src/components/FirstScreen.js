2c4780a2da58d7c7552ce3bbe9e0a195b58bfd50
Ekaterina Grishina <grischina.kotya17@gmail.com>
2024-03-29 12:13:38 +0100

    Change template for component FirsctScreen for better adaptive and styling

--- src/components/FirstScreen.js
+++ src/components/FirstScreen.js
@@ -4,39 +4,24 @@ import { Link } from "react-router-dom";
 export default function FirstScreen({ type, imgPath, specialImgClass, title, small, smallStyle, btn, btns, btnStyle }) {
 
     return (
-        (type === 'center' ? (
-            <div className='main-info'>
-                <h1>{title}</h1>
-                <div className='main-column'>
-                    <small>{small}</small>
-                    <div className='main-img'>
-                        <img src={imgPath} alt=''></img>
-                    </div>
-                    <div className='main-btnblock'>
+        <div className={type ? 'first-screen center' : 'first-screen nocenter'}>
+            <h1>{title}</h1>
+            <p style={smallStyle}>{small}</p>
+            <div className='first-img'>
+                <img className={specialImgClass} src={imgPath} alt=''></img>
+            </div>
+            <div className='first-btnblock'>
+                {btn ? <Link to="">
+                    <button className={btnStyle ? btnStyle : 'main-btn'}>{btn} {btnStyle && <i className="fas fa-arrow-right"></i>}</button>
+                </Link> : btns.map((i, idx) => {
+                    return (
                         <Link to="">
-                            <button className={btnStyle ? btnStyle : 'main-btn'}>{btn} {btnStyle && <i className="fas fa-arrow-right"></i>}</button>
+                            <button key={idx} className={i.class ? i.class + ' main-btn' : 'main-btn'}>{i.text}</button>
                         </Link>
-                    </div>
-                </div>
+                    )
+                })}
             </div>
-        ) : (
-            <div className='main-info'>
-                <div className='main-info-flex'>
-                    <div className='main-info-column'>
-                        <h1>{title}</h1>
-                        <small style={smallStyle}>{small}</small>
-                        <div className='main-btnblock'>
-                            {btn ? <button className='main-btn'>{btn}</button> : btns.map((i, idx) => {
-                                return (
-                                    <button key={idx} className={i.class ? i.class + ' main-btn' : 'main-btn'}>{i.text}</button>
-                                )
-                            })}</div>
-                    </div>
-                    <div className='main-img'>
-                        <img className={specialImgClass} src={imgPath} alt=''></img>
-                    </div>
-                </div>
-            </div>
-        ))
+        </div>
+
     );
 }

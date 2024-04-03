80310e2a1e19ea266d9241857f66e35db33b72ea
Ekaterina Grishina <grischina.kotya17@gmail.com>
2024-04-03 11:45:49 +0200

    Added style for adapting component Map

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,11 +3,11 @@ import React from "react";
 export default function Map({ classInfo, anchorLink }) {
     return (
         <div className="map-container">
-            <div className={classInfo ? "map-info " + classInfo : "map-info"}>
+            <div className={classInfo ? "map-info " + classInfo : "map-info left"}>
                 <h6>Свяжитесь с нами</h6>
                 <div className="info">
                     <small>Наш адрес</small>
-                    <p>г. Новосибирск, <br></br>ул. Бердский тупик 9/1</p>
+                    <p>г. Новосибирск, ул. Бердский тупик 9/1</p>
                     <small>Телефон</small>
                     <p>+7 869 468 48 68</p>
                     <small>Время работы</small>
@@ -18,9 +18,9 @@ export default function Map({ classInfo, anchorLink }) {
                 </a>
             </div>
 
-            <section>
-                <iframe className={classInfo ? classInfo : undefined} title="map" src="https://yandex.ru/map-widget/v1/?ll=83.051717%2C54.836438&mode=search&ol=geo&ouri=ymapsbm1%3A%2F%2Fgeo%3Fdata%3DCgoxNTk4MTkxOTM4EkbQoNC-0YHRgdC40Y8sINCd0L7QstC-0YHQuNCx0LjRgNGB0LosINCR0LXRgNC00YHQutC40Lkg0YLRg9C_0LjQuiwgOS8xIgoNcBmmQhUgWFtC&z=17.17" frameBorder="1" allowFullScreen={true}></iframe>
-            </section>
+            <div className={classInfo ? "map " + classInfo : "map left"} >
+                <iframe title="map" src="https://yandex.ru/map-widget/v1/?ll=83.051717%2C54.836438&mode=search&ol=geo&ouri=ymapsbm1%3A%2F%2Fgeo%3Fdata%3DCgoxNTk4MTkxOTM4EkbQoNC-0YHRgdC40Y8sINCd0L7QstC-0YHQuNCx0LjRgNGB0LosINCR0LXRgNC00YHQutC40Lkg0YLRg9C_0LjQuiwgOS8xIgoNcBmmQhUgWFtC&z=17.17" frameBorder="1" allowFullScreen={true} scroll="false"></iframe>
+            </div>
         </div>
     )
 }

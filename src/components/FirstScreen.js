f2c23a1f18299db1b55a30d374f0fd115d4b6ec5
Ekaterina Grishina <grischina.kotya17@gmail.com>
2024-04-04 19:32:11 +0200

    Added new libraries for anchor link

--- src/components/FirstScreen.js
+++ src/components/FirstScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import { HashLink } from 'react-router-hash-link';
 
 export default function FirstScreen({ type, imgPath, specialImgClass, title, small, smallStyle, btn, btns, btnStyle }) {
 
@@ -11,12 +12,12 @@ export default function FirstScreen({ type, imgPath, specialImgClass, title, sma
                 <img className={specialImgClass} src={imgPath} alt=''></img>
             </div>
             <div className='first-btnblock'>
-                {btn ? <Link to="">
+                {btn ? <HashLink to={window.location.pathname + "#anchor-form"}>
                     <button className={btnStyle ? btnStyle : 'main-btn'}>{btn} {btnStyle && <i className="fas fa-arrow-right"></i>}</button>
-                </Link> : btns.map((i, idx) => {
+                </HashLink> : btns.map((i, idx) => {
                     return (
-                        <Link to="">
-                            <button key={idx} className={i.class ? i.class + ' main-btn' : 'main-btn'}>{i.text}</button>
+                        <Link to={i.link} key={idx}>
+                            <button className={i.class ? i.class + ' main-btn' : 'main-btn'}>{i.text}</button>
                         </Link>
                     )
                 })}

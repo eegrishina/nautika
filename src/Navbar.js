e5efb43c5d9d7f80cfaa47d6efa6ca57e3549d85
Ekaterina Grishina <grischina.kotya17@gmail.com>
2024-05-03 13:54:14 +0200

    Add closing event for small screens menu

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,26 +5,22 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
     const [burgerStyle, setBurgerStyle] = useState(false);
 
+    window.addEventListener('click', (e) => {
+        if (!e.target.closest('.header') || e.target.closest('A')) {
+            setBurgerStyle(false);
+        }
+    })
+
     return (
         <div className="navbar">
             <div className={burgerStyle ? 'header burger-visible' : 'header'}>
                 <Link to="/">
                     <img className='header-logo' src="./images/icons/new-logo.png" alt="Nautika"></img>
                 </Link>
-                <div className="burger-menu" onClick={() => setBurgerStyle(!burgerStyle)}><span></span></div>
+                <div className="burger-menu" onClick={() => burgerStyle ? setBurgerStyle(false) : setBurgerStyle(true)}><span></span></div>
                 <nav className='header-menu'>
                     <ul>
-                        <li>
-                            <details>
-                                <summary><Link to='/services'>Услуги</Link></summary>
-                                <ul>
-                                    <li><Link to='/education'>Обучение</Link></li>
-                                    <li><Link to='/walks'>Прогулки</Link></li>
-                                    <li><Link to='/rent'>Аренда</Link></li>
-                                    <li><Link to='/certificates'>Фотосессии</Link></li>
-                                </ul>
-                            </details>
-                        </li>
+                        <li><Link to='/services'>Услуги</Link></li>
                         <li><Link to='/certificates'>Сертификаты</Link></li>
                         <li><Link to='/about'>О нас</Link></li>
                         <li><Link to='/weather'>Погода</Link></li>
